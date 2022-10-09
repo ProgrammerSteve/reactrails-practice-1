405bdfb2f33d3ee5420212ff48a3814fcf4d8b52
ProgrammerSteven <Stevenmb1995@gmail.com>
2022-10-08 23:40:29 -0500

    esbuild doesn't work, so I am trying webpack

---
+++ app/javascript/application.js
@@ -0,0 +1,6 @@
+// Entry point for the build script in your package.json
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
